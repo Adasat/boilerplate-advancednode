75df7fae1294fe43158f899c66cf81e9bf841275
Adasat <126713160+Adasat@users.noreply.github.com>
2023-08-18 12:11:41 +0000

    In the third exercise

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const path = require('path');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy; // If using local strategy
 
 const app = express();
 
@@ -14,11 +17,20 @@ app.set('views', './views/pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'secret-key',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 
 app.route('/').get((req, res) => {
-   res.render('index')
+   res.render('index', { title: 'Hello', message: 'Please log in' })
 });
 
 const PORT = process.env.PORT || 3000;
