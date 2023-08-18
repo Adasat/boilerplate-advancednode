10f35d4da041eb4f127199fa7086bc12cb01bb79
Adasat <126713160+Adasat@users.noreply.github.com>
2023-08-18 11:50:45 +0000

    Primer ejercicio hecho!

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const path = require('path');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+console.log(__dirname)
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
 
+
+app.route('/').get((req, res) => {
+   res.render('index')
 });
 
 const PORT = process.env.PORT || 3000;
